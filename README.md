bd48f78deb3e5cb879da0236a8024fdf24d2a260
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-11-30 12:18:31 -0600

    Minor changes in shapes, implemented sensors and update README.

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Core collision systems and vehicle physics library for Python
 
 # References
 
-1. Concise tutorial for 2-dimensional convex polygons collision detection using the separating axis theorem. - HackMD. (s. f.). HackMD. [https://hackmd.io/@US4ofdv7Sq2GRdxti381_A/ryFmIZrsl](https://hackmd.io/@US4ofdv7Sq2GRdxti381_A/ryFmIZrsl)
-2. pygame 2d car tutorial. (2018, 10 marzo). [http://rmgi.blog./pygame-2d-car-tutorial.html](http://rmgi.blog./pygame-2d-car-tutorial.html)
+1. Möllmann, L. (2017, 15 marzo). Concise tutorial for 2-dimensional convex polygons collision detection using the separating axis theorem. - HackMD. HackMD. [https://hackmd.io/@US4ofdv7Sq2GRdxti381_A/ryFmIZrsl](https://hackmd.io/@US4ofdv7Sq2GRdxti381_A/ryFmIZrsl) 
+2. PyGame 2D Car Tutorial. (2018, 10 marzo). RMGI Blog. [http://rmgi.blog./pygame-2d-car-tutorial.html](http://rmgi.blog./pygame-2d-car-tutorial.html)
--- pycarphysics/collisions/shapes.py
+++ pycarphysics/collisions/shapes.py
@@ -1,23 +1,26 @@
 import abc
 import math
+import typing
 
 import numpy as np
 
 from pycarphysics.transform import escalation, rotation
 
+Verx = typing.TypeVar('Verx', tuple[int, int], tuple[float, float], list[int], list[float], np.ndarray)
+
 
 class Shape(abc.ABC):
-    def scale(self, axis): ...
+    def scale(self, axis: Verx): ...
 
-    def translate(self, axis): ...
+    def translate(self, axis: Verx): ...
 
-    def rotate(self, angle): ...
+    def rotate(self, angle: int | float, origin=None): ...
 
 
 class PolygonShape(Shape):
     def __init__(self, points, origin):
         self.__points = np.array(points, dtype=np.float64)
-        self.__origin = origin
+        self.__origin = np.array(origin, dtype=np.float64)
         self.__angle = math.radians(0)
 
     @property
@@ -25,7 +28,7 @@ class PolygonShape(Shape):
         return self.__angle
 
     @angle.setter
-    def angle(self, angle):
+    def angle(self, angle: Verx):
         angle = self.angle - angle
         self.rotate(angle)
 
@@ -34,7 +37,7 @@ class PolygonShape(Shape):
         return np.array(self.__origin)
 
     @position.setter
-    def position(self, axis):
+    def position(self, axis: Verx):
         translate = np.array(axis) - self.origin
         self.translate(translate)
 
@@ -46,27 +49,40 @@ class PolygonShape(Shape):
     def origin(self):
         return self.__origin
 
-    def scale(self, axis):
+    def scale(self, axis: Verx):
         self.__points = np.dot(self.points - self.origin, escalation(axis)) + self.origin
 
-    def translate(self, axis):
+    def translate(self, axis: Verx):
         self.__origin += axis
         self.__points += axis
 
-    def rotate(self, angle):
+    def rotate(self, angle: int | float, origin=None):
         r = math.radians(angle)
 
+        if origin is None:
+            origin = self.origin
+
         self.__angle = self.angle - angle
-        self.__points = np.dot(self.points - self.origin, rotation(r)) + self.origin
+        self.__points = np.dot(self.points - origin, rotation(r)) + origin
 
 
 class SquareShape(PolygonShape):
-    def __init__(self, size: int | float):
-        super().__init__([(0, 0), (1 * size, 0), (1 * size, 1 * size), (0, 1 * size)], (0.5 * size, 0.5 * size))
+    def __init__(self, size: int | float, origin=None):
+        if origin is None:
+            origin = (0.5 * size, 0.5 * size)
+
+        super().__init__(
+            [(0, 0), (1 * size, 0), (1 * size, 1 * size), (0, 1 * size)], origin
+        )
 
 
 class RectangleShape(PolygonShape):
-    def __init__(self, size: tuple[int, int] | tuple[float, float]):
+    def __init__(self, size: tuple[int, int] | tuple[float, float], origin=None):
         width, height = size
-        super().__init__([(0, 0), (1 * width, 0), (1 * width, 1 * height), (0, 1 * height)],
-                         (0.5 * width, 0.5 * height))
+
+        if origin is None:
+            origin = (0.5 * width, 0.5 * height)
+
+        super().__init__(
+            [(0, 0), (1 * width, 0), (1 * width, 1 * height), (0, 1 * height)], origin
+        )
